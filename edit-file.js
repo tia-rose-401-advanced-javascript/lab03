9ec537b1f58d5e3d8f4e147d5c5bb8e5cb3acd65
Tia Rose <shortyshort789@aol.com>
2019-05-22 16:59:49 -0700

    reading contents of file one. Have files reading in order. Still need to test, need to read contents of other files

--- edit-file.js
+++ edit-file.js
@@ -6,12 +6,13 @@ var random = Math.random();
 
 fs.readFile('./files/test.txt', 'utf8', (error, data) => {
   if(error) throw error;
-  console.log(data);
+  console.log(`reading ${data}`);
+  console.log(data.length);
 });
 
 fs.writeFile('./files/test.txt', `${random}`, (error) => {
   if (error) throw error;
-  console.log('getting there');
+  console.log(`writing ${random}`);
 });
 
 
--- index.js
+++ index.js
@@ -2,45 +2,14 @@
 
 const fileReader = require('./lib/reader.js');
 
+// Obtain and assert input
+let files = process.argv.slice(2);
 
-let args = process.argv;
-
-if(args.length < 2){
-  console.error('Not enough args');
-  return;
-}
-let sourceFile = args[2];
-let destFile = args[3];
-let operation = args [4];
-
-
-for(let i = 0; i < args.length; i++){
-  console.log(args[i]);
+if( ! (files instanceof Array && files.length) ) {
+  throw new Error('Invalid Args');
 }
 
-// let file = `${__files}/test.txt`;
-// console.log(file);
-
-// let myattempt = (error, data) => {
-//   if(error) {throw error;}
-//   console.log(data)
-// };
-
-// try{
-//   fileReader(file, myattempt);
-// }catch(e){
-//   console.error(e);
-// }
-
-
-// // Obtain and assert input
-// let files = process.argv.slice(2);
-
-// if( ! (files instanceof Array && files.length) ) {
-//   throw new Error('Invalid Args');
-// }
-
-// fileReader(files, (err,data) => {
-//   if ( err ) { throw err; }
-//   console.log('From Callback:', data);
-// });
+fileReader(files, (err,data) => {
+  if ( err ) { throw err; }
+  console.log('From Callback:', data);
+});
